c35ecba8fc05eb4a58e3f4b10be7fe1c789991eb
mohamedibrahim5 <mohamedibrahimm355@gmail.com>
2025-06-17 20:29:14 +0300

    Added service provider for online store

--- project/settings.py
+++ project/settings.py
@@ -120,11 +120,13 @@ if DEBUG:
     #     },
     # }
 
-    REDIS_HOST = os.getenv("REDIS_HOST")
-    REDIS_PORT = os.getenv("REDIS_PORT")
-    REDIS_DB = os.getenv("REDIS_DB")
+    REDIS_USER = os.getenv("REDIS_USER", "default")  # Railway default
     REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-    REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+    REDIS_DB = os.getenv("REDIS_DB", 0)
+
+    REDIS_URL = f"redis://{REDIS_USER}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
     CHANNEL_LAYERS = {
         "default": {
@@ -134,6 +136,7 @@ if DEBUG:
             },
         },
     }
+
 
 
     
