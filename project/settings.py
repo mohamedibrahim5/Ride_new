8418dfc453dbcee0124d01a4623457bbcb36453f
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2025-08-01 20:19:21 +0300

    Added Filter Date for Customers

--- project/settings.py
+++ project/settings.py
@@ -217,8 +217,7 @@ LANGUAGES = [
 ]
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
-USE_I18N = True
+TIME_ZONE = 'Africa/Cairo'  # or your local timezoneUSE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOCALE_PATHS = [BASE_DIR / "locale"]  # Ensure you have this line
