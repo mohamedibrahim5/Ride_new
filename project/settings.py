a596a8e9f3db2ab22b43e684769954f99c9a5cc8
Mohamed Hamdy <117542420+1MohamedHamdy@users.noreply.github.com>
2025-08-28 13:56:28 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -18,8 +18,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ridenew-production-d802.up.railway.app",
-    "http://ridenew-production-d802.up.railway.app",
+    "https://ridenew-production-a6fe.up.railway.app",
+    "https://ridenew-production-a6fe.up.railway.app",
 ]
 
 INSTALLED_APPS = [
