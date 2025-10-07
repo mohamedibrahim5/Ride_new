550e3bb3df10586036eac35632cfee93c4797e30
mohamed ibrahim <mohamedibrahimm355@gmail.com>
2025-10-07 11:59:27 +0300

    testing deep link

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('quick-actions/', views.quick_actions, name='quick_actions'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    # path('quick-actions/', views.quick_actions, name='quick_actions'),
     # .well-known endpoints for app/universal links
     path('.well-known/apple-app-site-association', views.apple_app_site_association, name='apple-app-site-association'),
     path('.well-known/assetlinks.json', views.android_assetlinks, name='android-assetlinks'),
