a30ab9c31fbd288fbcce60ba99c8d2f40ca1a52b
Mohamed Hamdy <117542420+1MohamedHamdy@users.noreply.github.com>
2025-11-03 00:30:35 +0200

    Modify CSRF_TRUSTED_ORIGINS in settings.py
    Updated CSRF_TRUSTED_ORIGINS to include new production URLs.

--- project/settings.py
+++ project/settings.py
@@ -18,8 +18,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ridenew-production-ff37.up.railway.app",
-    "https://ridenew-production-ff37.up.railway.app",
+    "http://ride-production-f9cd.up.railway.app",
+    "https://ride-production-f9cd.up.railway.app",
 ]
 
 INSTALLED_APPS = [
